dbc425aa5b402b74b5b2c5c988f816380e783ef3
ethanae <eemandien@mweb.co.za>
2020-05-12 16:27:25 +0200

    - add more type examples

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 class MyClass {
 }
-console.log('THE TYPE IS ', "MyClass");
+console.log('THE TYPE IS ', "String");
+console.log('THE TYPE IS ', "Boolean");
+console.log('THE TYPE IS ', "Number");
+console.log('THE TYPE IS ', "Object");
+console.log('THE TYPE IS ', "Array");
+console.log('THE TYPE IS ', "MySuperType");
+console.log('THE TYPE IS ', "ISuperInterface");
--- test.ts
+++ test.ts
@@ -1,4 +1,17 @@
 class MyClass{
 }
 
-console.log('THE TYPE IS ', nameof<MyClass>())
+type MySuperType = {
+
+}
+
+interface ISuperInterface {
+
+}
+console.log('THE TYPE IS ', nameof<String>())
+console.log('THE TYPE IS ', nameof<Boolean>())
+console.log('THE TYPE IS ', nameof<Number>())
+console.log('THE TYPE IS ', nameof<Object>())
+console.log('THE TYPE IS ', nameof<Array<string>>())
+console.log('THE TYPE IS ', nameof<MySuperType>())
+console.log('THE TYPE IS ', nameof<ISuperInterface>())
